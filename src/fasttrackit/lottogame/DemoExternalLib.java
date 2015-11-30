999ad98ed7bf24a89b8d3a5beb566e9b5678b633
Ionel Condor <ionel-condor@yahoo.com>
2015-12-01 01:11:56 +0200

    rename

--- src/fasttrackit/lottogame/DemoExternalLib.java
+++ src/fasttrackit/lottogame/DemoExternalLib.java
@@ -7,9 +7,10 @@ import ionel.demolib.*;
 /**
  * Created by condor on 21/11/15.
  */
-public class Ionel  {
+public class DemoExternalLib  {
     public static void main(String[] argv) {
-         List l1 = new DenissaList();
+         List<String> l1;
+        l1 = new DenissaList();
         l1.add("ana");
         l1.add("are");
         l1.add("mere");
